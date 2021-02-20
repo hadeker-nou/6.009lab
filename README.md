78cbabeb35a8f35e119666f28734f6d4f11d65cc
Dmytro Shaban <10shaban72@gmail.com>
2021-02-20 12:29:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Fundamentals of Programming (Fall 2020 and spring 2021)
+=======================================
+
+This is a MIT 6.009 course.
---
+++ labs/lab0/lab.py
@@ -0,0 +1,90 @@
+# No Imports Allowed!
+
+
+def backwards(sound):
+    raise NotImplementedError
+
+
+def mix(sound1, sound2, p):
+    raise NotImplementedError
+
+
+def echo(sound, num_echos, delay, scale):
+    raise NotImplementedError
+
+
+def pan(sound):
+    raise NotImplementedError
+
+
+def remove_vocals(sound):
+    raise NotImplementedError
+
+
+# below are helper functions for converting back-and-forth between WAV files
+# and our internal dictionary representation for sounds
+
+import io
+import wave
+import struct
+
+def load_wav(filename):
+    """
+    Given the filename of a WAV file, load the data from that file and return a
+    Python dictionary representing that sound
+    """
+    f = wave.open(filename, 'r')
+    chan, bd, sr, count, _, _ = f.getparams()
+
+    assert bd == 2, "only 16-bit WAV files are supported"
+
+    left = []
+    right = []
+    for i in range(count):
+        frame = f.readframes(1)
+        if chan == 2:
+            left.append(struct.unpack('<h', frame[:2])[0])
+            right.append(struct.unpack('<h', frame[2:])[0])
+        else:
+            datum = struct.unpack('<h', frame)[0]
+            left.append(datum)
+            right.append(datum)
+
+    left = [i/(2**15) for i in left]
+    right = [i/(2**15) for i in right]
+
+    return {'rate': sr, 'left': left, 'right': right}
+
+
+def write_wav(sound, filename):
+    """
+    Given a dictionary representing a sound, and a filename, convert the given
+    sound into WAV format and save it as a file with the given filename (which
+    can then be opened by most audio players)
+    """
+    outfile = wave.open(filename, 'w')
+    outfile.setparams((2, 2, sound['rate'], 0, 'NONE', 'not compressed'))
+
+    out = []
+    for l, r in zip(sound['left'], sound['right']):
+        l = int(max(-1, min(1, l)) * (2**15-1))
+        r = int(max(-1, min(1, r)) * (2**15-1))
+        out.append(l)
+        out.append(r)
+
+    outfile.writeframes(b''.join(struct.pack('<h', frame) for frame in out))
+    outfile.close()
+
+
+if __name__ == '__main__':
+    # code in this block will only be run when you explicitly run your script,
+    # and not when the tests are being run.  this is a good place to put your
+    # code for generating and saving sounds, or any other code you write for
+    # testing, etc.
+
+    # here is an example of loading a file (note that this is specified as
+    # sounds/hello.wav, rather than just as hello.wav, to account for the
+    # sound files being in a different directory than this file)
+    hello = load_wav('sounds/hello.wav')
+
+    # write_wav(backwards(hello), 'hello_reversed.wav')
